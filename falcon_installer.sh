28381e997254101c1b182f8abcb8c2455ab0f06c
nutellaBear <48599863+LalithShiyam@users.noreply.github.com>
2022-01-30 22:33:00 +0100

    Update falcon_installer.sh

--- falcon_installer.sh
+++ falcon_installer.sh
@@ -4,10 +4,16 @@ pip install halo==0.0.31 SimpleITK==2.1.1
 echo '[2] Downloading required files IBM cloud storage...'
 if [[ "$OSTYPE" == "linux-gnu"* ]]; then
         wget "https://falcon.s3.eu-de.cloud-object-storage.appdomain.cloud/FALCON-files.zip"  
+        sudo apt-get install cmake pkg-config
+        sudo apt install git
+        sudo apt install python2
 elif [[ "$OSTYPE" == "darwin"* ]]; then
         /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
         brew install wget
         wget "https://falcon.s3.eu-de.cloud-object-storage.appdomain.cloud/FALCON-files.zip"  
+        brew install pkg-config
+        brew install git
+        brew install python@2
 fi
 echo '[3] Unzipping FALCON files...'
 unzip FALCON-files.zip
@@ -20,19 +26,16 @@ falcon_bin=$falcon_dir/'bin'
 root_path='/usr/local/bin'
 falcon_src=$main_dir/'src'/'run_falcon.py'
 
-
 echo '[5] Setting up symlinks for dependencies...'
 sudo ln -s $falcon_bin/'c3d' $root_path/'c3d'
 sudo ln -s $falcon_bin/'greedy' $root_path/'greedy'
 echo '[6] Building dcm2niix from source...'
-sudo apt-get install cmake pkg-config
-sudo apt install git
+
 git clone https://github.com/rordenlab/dcm2niix.git
 cd dcm2niix
 mkdir build && cd build
 cmake -DZLIB_IMPLEMENTATION=Cloudflare -DUSE_JPEGLS=ON -DUSE_OPENJPEG=ON ..
 sudo make install 
-sudo apt install python2
 echo '[7] Installing fsl, please answer the required questions via the terminal...'
 python2 $falcon_bin/fslinstaller.py 
 sudo chmod +x $falcon_src
