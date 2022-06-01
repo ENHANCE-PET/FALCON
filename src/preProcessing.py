0ee4f922555d559e45040074d3cd1bf2eacaa677
Keyn34 <gutschmayer@gmail.com>
2022-06-01 12:12:37 +0200

    - Changed computeMI to compute_MI - compute_MI now returns positive MI - Changed current_MI < MI_threshold to current_MI > MI_threshold to account for above change - Changed determine_starting_frame_by_threshold to find_start_frame - Using fileOP make_dir now - Sample size is now calculated based on amount of available files (20%) - Added downsample_image() which combines blurring and resampling - Imported mpire for future use

--- src/preProcessing.py
+++ src/preProcessing.py
@@ -18,6 +18,7 @@ import subprocess
 import os
 import SimpleITK as sitk
 import fileOp as fop
+import mpire
 
 
 def blur_image_c3d(input_image: str, output_image: str, shrink_factor: int) -> None:
@@ -29,10 +30,10 @@ def blur_image_c3d(input_image: str, output_image: str, shrink_factor: int) -> N
     """
     gauss_variance = (shrink_factor / 2) ** 2
     gauss_variance = int(gauss_variance)
-    cmd_to_smooth = f"c3d {input_image} " \
-                    f"-smooth-fast {gauss_variance}x{gauss_variance}x{gauss_variance}vox " \
-                    f"-o {output_image}"
-    subprocess.run(cmd_to_smooth, shell=True, capture_output=True)
+    run_cmd = f"c3d {input_image} " \
+              f"-smooth-fast {gauss_variance}x{gauss_variance}x{gauss_variance}vox " \
+              f"-o {output_image}"
+    subprocess.run(run_cmd, shell=True, capture_output=True)
 
 
 def resample_image_c3d(input_image: str, output_image: str, shrink_factor: int) -> None:
@@ -43,31 +44,50 @@ def resample_image_c3d(input_image: str, output_image: str, shrink_factor: int) 
     :param shrink_factor: kernel modifier
     """
     shrink_percentage = str(int(100 / shrink_factor))
-    cmd_to_run = f"c3d {input_image} " \
-                 f"-resample {shrink_percentage}x{shrink_percentage}x{shrink_percentage}% " \
-                 f"-o {output_image}"
+    run_cmd = f"c3d {input_image} " \
+              f"-resample {shrink_percentage}x{shrink_percentage}x{shrink_percentage}% " \
+              f"-o {output_image}"
 
-    subprocess.run(cmd_to_run, shell=True, capture_output=True)
+    subprocess.run(run_cmd, shell=True, capture_output=True)
 
 
-def computeMI(image1: str, image2: str) -> float:
+def downsample_image(input_image: str, output_directory: str, shrink_factor: int) -> str:
+    """
+    Creates a downsampled version of the input image based on the downsampling_factor
+    :param input_image: image to downsample
+    :param output_directory: folder to write files to
+    :param shrink_factor: shrink factor to downsample image by
+    :return: path to the downsampled input_image
+    """
+    input_image_name = os.path.basename(input_image)
+
+    # Blur and resample the input image
+    input_image_blurred = os.path.join(output_directory, f"{shrink_factor}x_blurred_{input_image_name}")
+    blur_image_c3d(input_image, input_image_blurred, shrink_factor)
+    input_image_downsampled = os.path.join(output_directory, f"{shrink_factor}x_resampled_{input_image_name}")
+    resample_image_c3d(input_image_blurred, input_image_downsampled, shrink_factor)
+
+    return input_image_downsampled
+
+
+def compute_MI(image1: str, image2: str) -> float:
     """
     Compares two images and computes their mutual information
     :param image1: first image of image pair to compare
     :param image2: second image of image pair to compare
-    :return: Computed mutual information as float
+    :return: Computed mutual information as float and absolute value (only positive)
     :rtype: float
     """
     # SimpleITK
-    img1 = sitk.ReadImage(image1)
-    img2 = sitk.ReadImage(image2)
+    sitk_image1 = sitk.ReadImage(image1)
+    sitk_image2 = sitk.ReadImage(image2)
     registration_method = sitk.ImageRegistrationMethod()
     registration_method.SetMetricAsMattesMutualInformation()
-    MI = registration_method.MetricEvaluate(img1, img2)
+    MI = abs(registration_method.MetricEvaluate(sitk_image1, sitk_image2))
     return MI
 
 
-def determine_starting_frame_by_threshold(folder: str) -> int:
+def find_start_frame(folder: str) -> int:
     """
     Determines the starting frame of a 4D series by a thresholding approach.
     :param folder: Folder where multiple 3D volumes of a 4D series are located.
@@ -79,21 +99,16 @@ def determine_starting_frame_by_threshold(folder: str) -> int:
     files = fop.get_files(folder, "*.nii*")
 
     # create pyramid folder to dump images to
-    pyramid_path = os.path.join(folder, "pyramid")
-    if not os.path.exists(pyramid_path):
-        os.mkdir(pyramid_path)
+    pyramid_path = fop.make_dir(folder, "pyramid")
 
     # blur and resample the reference frame
     file_index_reference = len(files) - 1
     reference_image = os.path.join(folder, files[file_index_reference])
-    reference_image_blurred = os.path.join(pyramid_path, f"blurred_{files[file_index_reference]}")
-    blur_image_c3d(reference_image, reference_image_blurred, 4)
-    reference_image_resampled = os.path.join(pyramid_path, f"resampled_{files[file_index_reference]}")
-    resample_image_c3d(reference_image_blurred, reference_image_resampled, 4)
+    reference_image_resampled = downsample_image(reference_image, pyramid_path, 4)
 
     # get threshold from MIs of original files
-    sample_size = 4
     sample_stop = len(files)
+    sample_size = int(sample_stop * 0.20)
     sample_start = sample_stop - sample_size
     print(f"Computing threshold between file {files[sample_start]} and {files[sample_stop - 1]}")
 
@@ -101,7 +116,7 @@ def determine_starting_frame_by_threshold(folder: str) -> int:
     reference_MI_sum = 0
     for sample_index in range(sample_start, sample_stop):
         current_image = os.path.join(folder, files[sample_index])
-        current_MI = computeMI(reference_image, current_image)
+        current_MI = compute_MI(reference_image, current_image)
         reference_MI_sum = reference_MI_sum + current_MI
     MI_threshold = reference_MI_sum / sample_size
     print(f"MI threshold: {MI_threshold}")
@@ -111,15 +126,12 @@ def determine_starting_frame_by_threshold(folder: str) -> int:
     for file_index in range(len(files)):
         current_image = os.path.join(folder, files[file_index])
         # Blur and resample the frame to investigate
-        current_image_blurred = os.path.join(pyramid_path, f"blurred_{files[file_index]}")
-        blur_image_c3d(current_image, current_image_blurred, 4)
-        current_image_resampled = os.path.join(pyramid_path, f"resampled_{files[file_index]}")
-        resample_image_c3d(current_image_blurred, current_image_resampled, 4)
+        current_image_resampled = downsample_image(current_image, pyramid_path, 4)
 
         # Compute MI
-        current_MI = computeMI(reference_image_resampled, current_image_resampled)
+        current_MI = compute_MI(reference_image_resampled, current_image_resampled)
 
-        if current_MI < MI_threshold:
+        if current_MI > MI_threshold:
             starting_frame = file_index
             print(f"Threshold: Starting frame found at index {starting_frame}, file {files[starting_frame]}")
             break
