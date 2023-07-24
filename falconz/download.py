13f6cdd3ef424adb3f6bd33b122088e797d7d692
lalithShiyam <lalith.shiyamsundar@meduniwien.ac.at>
2023-07-24 16:08:42 +0200

    Start building falcon v.2.0

---
+++ falconz/download.py
@@ -0,0 +1,108 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# ----------------------------------------------------------------------------------------------------------------------
+# Author: Lalith Kumar Shiyam Sundar
+# Institution: Medical University of Vienna
+# Research Group: Quantitative Imaging and Medical Physics (QIMP) Team
+# Date: 04.07.2023
+# Version: 0.1.0
+#
+# Description:
+# This module downloads the necessary binaries and models for the falconz.
+#
+# Usage:
+# The functions in this module can be imported and used in other modules within the pumaz to download the necessary
+# binaries and models for the falconz.
+#
+# ----------------------------------------------------------------------------------------------------------------------
+
+import logging
+import os
+import zipfile
+
+import requests
+
+from pumaz import constants
+
+from rich.console import Console
+from rich.progress import Progress, BarColumn, TextColumn, TimeRemainingColumn, FileSizeColumn, TransferSpeedColumn
+import time
+
+
+def download(item_name, item_path, item_dict):
+    """
+    Downloads the item (model or binary) for the current system.
+    :param item_name: The name of the item to download.
+    :param item_path: The path to store the item.
+    :param item_dict: The dictionary containing item info.
+    """
+    item_info = item_dict[item_name]
+    url = item_info["url"]
+    filename = os.path.join(item_path, item_info["filename"])
+    directory = os.path.join(item_path, item_info["directory"])
+
+    if not os.path.exists(directory):
+        logging.info(f" Downloading {directory}")
+
+        # show progress using rich
+        response = requests.get(url, stream=True)
+        total_size = int(response.headers.get("Content-Length", 0))
+        chunk_size = 1024 * 10
+
+        console = Console()
+        progress = Progress(
+            TextColumn("[bold blue]{task.description}"),
+            BarColumn(bar_width=None),
+            "[progress.percentage]{task.percentage:>3.0f}%",
+            "•",
+            FileSizeColumn(),
+            TransferSpeedColumn(),
+            TimeRemainingColumn(),
+            console=console,
+            expand=True
+        )
+
+        with progress:
+            task = progress.add_task("[white] Downloading system specific registration binaries", total=total_size)
+            for chunk in response.iter_content(chunk_size=chunk_size):
+                open(filename, "ab").write(chunk)
+                progress.update(task, advance=chunk_size)
+
+        # Unzip the item
+        progress = Progress(  # Create new instance for extraction task
+            TextColumn("[bold blue]{task.description}"),
+            BarColumn(bar_width=None),
+            "[progress.percentage]{task.percentage:>3.0f}%",
+            "•",
+            FileSizeColumn(),
+            TransferSpeedColumn(),
+            TimeRemainingColumn(),
+            console=console,
+            expand=True
+        )
+
+        with progress:
+            with zipfile.ZipFile(filename, 'r') as zip_ref:
+                total_size = sum((file.file_size for file in zip_ref.infolist()))
+                task = progress.add_task("[white] Extracting system specific registration binaries",
+                                         total=total_size)
+                # Get the parent directory of 'directory'
+                parent_directory = os.path.dirname(directory)
+                for file in zip_ref.infolist():
+                    zip_ref.extract(file, parent_directory)
+                    extracted_size = file.file_size
+                    progress.update(task, advance=extracted_size)
+
+        logging.info(f" {os.path.basename(directory)} extracted.")
+
+        # Delete the zip file
+        os.remove(filename)
+        print(f"{constants.ANSI_GREEN} Registration binaries - download complete. {constants.ANSI_RESET}")
+        logging.info(f" Registration binaries - download complete.")
+    else:
+        print(f"{constants.ANSI_GREEN} A local instance of the system specific registration binary has been detected. "
+              f"{constants.ANSI_RESET}")
+        logging.info(f" A local instance of registration binary has been detected.")
+
+    return os.path.join(item_path, item_name)
---
+++ setup.py
@@ -0,0 +1,60 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='falconz',
+    version='2.0.0',
+    author='Lalith Kumar Shiyam Sundar',
+    author_email='Lalith.shiyamsundar@meduniwien.ac.at',
+    description='FalconZ: A streamlined Python package for PET motion correction.',
+    python_requires='>=3.9',
+    long_description='FalconZ is a robust and comprehensive Python package that offers a simplified approach to PET ('
+                     'Positron Emission Tomography) motion correction. The software is equipped to handle both head '
+                     'and total-body scans, ensuring high-accuracy results in diverse settings. Built around the '
+                     'potent greedy registration toolkit, FalconZ serves as an efficient engine for registration '
+                     'tasks, providing precise alignment in PET imaging. The package has been designed with '
+                     'user-friendliness in mind, streamlining the implementation process of motion correction. '
+                     'Whether you are a researcher, healthcare professional, or an individual working with PET scans, '
+                     'FalconZ provides a seamless solution to your needs. It integrates effortlessly into your '
+                     'existing Python environment, and its straightforward usability makes it an indispensable tool '
+                     'for accurate PET scan analysis.',
+    url='https://github.com/QIMP-Team/FALCON',
+    license='GPLv3',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Healthcare Industry',
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python :: 3 :: Only',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Topic :: Scientific/Engineering :: Medical Science Apps.',
+    ],
+
+    keywords='PET motion correction, diffeomorphic imaging, image processing',
+    packages=find_packages(),
+    install_requires=[
+        'nibabel~=3.2.2',
+        'halo~=0.0.31',
+        'SimpleITK~=2.2.1',
+        'pydicom~=2.2.2',
+        'argparse~=1.4.0',
+        'numpy~=1.22.3',
+        'mpire~=2.3.3',
+        'openpyxl~=3.0.9',
+        'matplotlib',
+        'pyfiglet~=0.8.post1',
+        'natsort~=8.1.0',
+        'pillow>=9.2.0',
+        'colorama~=0.4.6',
+        'rich',
+        'pandas',
+        'dicom2nifti~=2.4.8',
+        'requests'
+    ],
+    entry_points={
+        'console_scripts': [
+            'falconz=falconz.falconz:main',
+        ],
+    },
+)
